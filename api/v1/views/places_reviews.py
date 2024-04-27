985e281c79e3ea9131546b89fdd6763d2ca61038
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-04-27 23:14:22 +0400

    Adapted pycode style

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -83,7 +83,8 @@ def put_review(review_id):
     except Exception as e:
         abort(400, 'Not a JSON')
     for key, value in data.items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated_at']:
+        if key not in ['id', 'user_id', 'place_id',
+                       'created_at', 'updated_at']:
             setattr(review, key, value)
     storage.save()
     return jsonify(review.to_dict())
